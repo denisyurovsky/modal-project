245fa30f5718e2845e0fce6a15076993f630119c
Denis Yurovsky <moon.wave578@gmail.com>
2022-08-23 15:04:37 +0300

    end

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modalButtons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+
+modalButtons.forEach(button => {
+  button.addEventListener('click', openModal);
+});
+
+btn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+document.addEventListener('keydown', e => {
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
